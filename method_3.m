aeec4403ca8b568433807320f25624786b5aef7c
ruchi-holker <ruchi.holker@gmail.com>
2025-06-11 22:21:32 +0530

    Update method_3.m

--- method_3.m
+++ method_3.m
@@ -1,36 +1,4 @@
 function feat_vec = method_3(EEG)
-%{
-% Approximate Entropy
-m = 3;
-ApEn = zeros(size(EEG, 1), 1);
-for ch = 1:size(EEG, 1)
-    eeg_band_data = real(ifft(EEG(ch, :)));
-
-    Xm = buffer(eeg_band_data, 3, 2);
-    Xm = Xm(:, 1:end-m+1);
-
-    r = 0.2*std(Xm, [], 1);
-    Cm = zeros(1, size(Xm, 2));
-    for i= 1:size(Xm, 2)
-        d = max(bsxfun(@minus, Xm, Xm(:, 1)), [], 1);
-        Cm(i) = sum((r(1) - d)>0)/(size(Xm, 2));
-    end
-    phi_m = mean(log(Cm));
-
-    Xm1 = buffer(eeg_band_data, 3+1, 2);
-    Xm1 = Xm1(:, 1:end-m+1);
-
-    r1 = 0.2*std(Xm1, [], 1);
-    Cm1 = zeros(1, size(Xm1, 2));
-    for i= 1:size(Xm1, 2)
-        d = max(bsxfun(@minus, Xm1, Xm1(:, 1)), [], 1);
-        Cm1(i) = sum((r1(1) - d)>0)/(size(Xm1, 2));
-    end
-    phi_m1 = mean(log(Cm1));
-
-    ApEn(ch) = phi_m - phi_m1;
-end
-%}
 % Permutation entropy
 m = 3;
 Pi = perms(1:m);  % Symbols
